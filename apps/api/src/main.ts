fe50a61e309cef67a801a55e9dbdff74f0635f8c
Tommy Larida <tommy.larida@nixs.com>
2026-09-22 18:26:56 +0200

    deploy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,36 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
-  new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: true, } );
+
+  // Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
+  // Railway needs PORT + 0.0.0.0
+  const port = process.env.PORT ?? 3001;
+
+  await app.listen(port, '0.0.0.0');
 }
+
 bootstrap();
